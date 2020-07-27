0a027a6dce9e2f92e452239c14f47bbf53c3849d
Manuel <manuelheller@outlook.com>
2020-07-27 12:59:20 +0200

    big step forward

--- test1.py
+++ test1.py
@@ -82,22 +82,14 @@ have_trait = {'James'}
 
 names = [name for name in set(people)]
 
-# Use information that either mother and father are both blank
-# (no parental information in the data set), or mother and
-# father will both refer to other people in the people dictionary.
-#parents = []
-#for name in set(people):
-#    if people[name]['mother'] == None:
-#        parents.append(0)
-#    else:
-#        parents.append(1)
-
-mother = []
+genes = []
 for name in set(people):
-    if people[name]['mother'] == None:
-        mother.append(None)
+    if name in one_gene:
+        genes.append(1)
+    elif name in two_genes:
+        genes.append(2)
     else:
-        mother.append(people[name]['mother'])
+        genes.append(0)
 
 mother_genes = []
 for name in set(people):
@@ -123,37 +115,115 @@ for name in set(people):
     else:
         father_genes.append(0)
 
-father = []
-for name in set(people):
-    if people[name]['father'] == None:
-        father.append(None)
-    else:
-        father.append(people[name]['father'])
-
-genes = []
-for name in set(people):
-    if name in one_gene:
-        genes.append(1)
-    elif name in two_genes:
-        genes.append(2)
-    else:
-        genes.append(0)
-
 trait = []
 for name in set(people):
     if name in have_trait:
         trait.append(True)
     else: trait.append(False)
 
-listofkeys = ('name', 'mother', 'mother_genes', 'father', 'father_genes', 'genes', 'trait')
-listofvalues = (names, mother, mother_genes, father, father_genes, genes, trait)
+
+
+listofkeys = ('name', 'genes', 'mother_genes', 'father_genes', 'trait')
+listofvalues = (names, genes, mother_genes, father_genes, trait)
 dictionary=dict(zip(listofkeys,listofvalues))
 df = pd.DataFrame(dictionary)
 df.set_index('name', inplace=True)
 
+def scenarios(row):
+    # No parent has gene
+    if (row['mother_genes'] == 0) and row['father_genes'] == 0:
+        return 'p_00'
+    # Both parents have one gene
+    if (row['mother_genes'] == 1) and row['father_genes'] == 1:
+        return 'p_11'
+    # Both parents have two genes
+    if (row['mother_genes'] == 2) and row['father_genes'] == 2:
+        return 'p_22'
+    # One parent has zero genes, one parent has one gene 
+    if (row['mother_genes'] == 0) and row['father_genes'] == 1:
+        return 'p_01'
+    if (row['mother_genes'] == 1) and row['father_genes'] == 0:
+        return 'p_01'
+    # One parent has zero genes, one parent has two genes 
+    if (row['mother_genes'] == 0) and row['father_genes'] == 2:
+        return 'p_02'
+    if (row['mother_genes'] == 2) and row['father_genes'] == 0:
+        return 'p_02'
+    # One parent has one gene, one parent has two genes 
+    if (row['mother_genes'] == 1) and row['father_genes'] == 2:
+        return 'p_12'
+    if (row['mother_genes'] == 2) and row['father_genes'] == 1:
+        return 'p_12'
+    
+# Scenarios:
+df['scenario'] = df.apply(scenarios, axis=1)
 #print(df)
 
-df['un_prob_gene'] = df.apply (lambda row: PROBS['gene'][row.genes], axis=1)
+def con_prob_0(row):
+    # Probability of getting no gene dependent on parents genes
+    if row['scenario'] == 'p_00':
+        return 0.99 * 0.99
+    if row['scenario'] == 'p_11':
+        return 0.5 * 0.5
+    if row['scenario'] == 'p_22':
+        return 0.01 * 0.01
+    if row['scenario'] == 'p_01':
+        return 0.50 * 0.99
+    if row['scenario'] == 'p_02':
+        return 0.99 * 0.01
+    if row['scenario'] == 'p_12':
+        return 0.50 * 0.01
+
+def con_prob_1(row):
+    # Probability of getting one gene dependent on parents genes
+    if row['scenario'] == 'p_00':
+        return 0.01 * 0.99 + 0.01 * 0.99
+    if row['scenario'] == 'p_11':
+        return 0.50 * 0.50 + 0.50 * 0.50
+    if row['scenario'] == 'p_22':
+        return 0.99 * 0.01 + 0.01 * 0.99
+    if row['scenario'] == 'p_01':
+        return 0.50 * 0.99 + 0.01 * 0.50
+    if row['scenario'] == 'p_02':
+        return 0.99 * 0.99 + 0.01 * 0.01
+    if row['scenario'] == 'p_12':
+        return 0.99 * 0.50 + 0.01 * 0.50
+
+def con_prob_2(row):
+    # Probability of getting two genes dependent on parents genes
+    if row['scenario'] == 'p_00':
+        return 0.01 * 0.01
+    if row['scenario'] == 'p_11':
+        return 0.5 * 0.5
+    if row['scenario'] == 'p_22':
+        return 0.99 * 0.99
+    if row['scenario'] == 'p_01':
+        return 0.50 * 0.01
+    if row['scenario'] == 'p_02':
+        return 0.99 * 0.01
+    if row['scenario'] == 'p_12':
+        return 0.50 * 0.99
+
+df['con_prob_0'] = df.apply(con_prob_0, axis=1)
+df['con_prob_1'] = df.apply(con_prob_1, axis=1)
+df['con_prob_2'] = df.apply(con_prob_2, axis=1)
+
+#df['un_prob_gene'] = df.apply (lambda row: PROBS['gene'][row.genes], axis=1)
+
+def prob_gene(row):
+    if row['scenario'] == None:
+        return PROBS['gene'][row.genes]
+    else:
+        if row.name in one_gene:
+            return con_prob_1(row)
+        elif row.name in two_genes:
+            return con_prob_2(row)
+        else:
+            return con_prob_0(row)
+                
+df['prob_gene'] = df.apply(prob_gene, axis=1)
+
+
 
 #df['un_prob_trait'] = df.apply (lambda row: PROBS['trait'][row.genes][row.trait], axis=1)
 # Combined probability of genes and trait is zero if we know the parent
